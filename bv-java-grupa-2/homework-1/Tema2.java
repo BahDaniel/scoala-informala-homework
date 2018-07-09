4f2f4705d33e4d1a0194ed6d569462acb248a5aa
BahDaniel <daniel.bahnareanu@gmail.com>
2018-07-09 20:23:49 +0300

    homework-1

---
+++ bv-java-grupa-2/homework-1/Tema2.java
@@ -0,0 +1,17 @@
+public class Tema2{
+	public static void main(String[] args) 
+	{
+                       
+         int a=4;
+         int b=6;
+         int c=1;
+
+         if(a<b&&a<c)
+         	System.out.println(a);
+         if(b<a&&b<c)
+         	System.out.println(b);
+         if(c<a&&c<b)
+         	System.out.println(c);
+         
+}
+}
